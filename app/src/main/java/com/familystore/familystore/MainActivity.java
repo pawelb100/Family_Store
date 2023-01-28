ab4d62128fe1f6b88c98502ea8d7b9ba04584e60
Paweł Borucki <pawel.borucki@wseiz.edu.pl>
2023-01-28 13:48:45 +0100

    Initial commit

---
+++ app/src/main/java/com/familystore/familystore/MainActivity.java
@@ -0,0 +1,14 @@
+package com.familystore.familystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
