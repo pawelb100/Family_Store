939b8cb65c90510fed1b7752664b3109bfd22bf7
Paweł Borucki <pawel.borucki@wseiz.edu.pl>
2023-07-10 17:40:18 +0200

    create FileManager, added list handling in FilesFragment

--- app/src/main/java/com/familystore/familystore/fragments/FilesFragment.java
+++ app/src/main/java/com/familystore/familystore/fragments/FilesFragment.java
@@ -4,13 +4,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.familystore.familystore.R;
+import com.familystore.familystore.adapters.FileListAdapter;
 import com.familystore.familystore.databinding.FragmentFilesBinding;
+import com.familystore.familystore.utils.FileManager;
 import com.familystore.familystore.viewmodels.MainViewModel;
 
 public class FilesFragment extends Fragment {
@@ -19,13 +21,21 @@ public class FilesFragment extends Fragment {
 
     private MainViewModel viewModel;
 
+    private FileManager fileManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentFilesBinding.inflate(inflater, container, false);
 
         ViewModelProvider viewModelProvider = new ViewModelProvider(requireActivity());
         viewModel = viewModelProvider.get(MainViewModel.class);
-        
+
+        fileManager = new FileManager(getContext());
+
+        FileListAdapter adapter = new FileListAdapter(getContext(), fileManager.getFiles(), file -> fileManager.installApk(file));
+        binding.rvFileList.setAdapter(adapter);
+        binding.rvFileList.setLayoutManager(new LinearLayoutManager(getContext()));
+
         return binding.getRoot();
     }
 
