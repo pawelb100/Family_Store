35c5581dc6b2141803350d7175be05d0e55986bf
maciejors <maciej.orslowski@gmail.com>
2025-09-28 20:37:53 +0200

    fix/#59 remove debug print

--- app/src/main/java/com/familystore/familystore/fragments/AppFragment.java
+++ app/src/main/java/com/familystore/familystore/fragments/AppFragment.java
@@ -41,7 +41,6 @@ public class AppFragment extends Fragment {
         assert getArguments() != null;
         int id = getArguments().getInt("appId", -1);
         // id == -1 when opened using deep link
-        System.out.println("HELLO FROM DEEPLINK ID " + id);
         if (id == -1) {
             Uri uri = activity.getIntent().getData();
             assert uri != null;
